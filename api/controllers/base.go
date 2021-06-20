0c4dc1aa1fb10783c59a3a530091c538942a823e
Haasin Farooq <haasin.farooq@tintash.com>
2021-06-21 03:59:21 +0500

    complete user model and controller

---
+++ api/controllers/base.go
@@ -0,0 +1,62 @@
+/*
+Build a simple Application which stores ToDo tasks with a due date for each user.
+The App should have jwt based authentication.
+You can use either use MySQL or PostgreSQL (recommended) as the database.
+The App should expose following endpoints for CRUD and auth operations:
+- Register a new user
+- User login
+- Add a Task
+- Edit a Task
+- Delete a Task
+- Get All Tasks for a user
+- Assign a Internal / External user a task by email address.
+  If the user doesn’t exist send them an email to sign up.
+  Once they signup that note should be assigned to them automatically.
+*/
+
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+
+	"github.com/haasin-farooq/todo-go-app/api/models"
+	"github.com/haasin-farooq/todo-go-app/api/responses"
+)
+
+type App struct {
+	Router *mux.Router
+	DB *gorm.DB
+}
+
+func (a *App) Initialize(DbHost, DbPort, DbUser, DbName, DbPassword string) {
+	var err error
+
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+
+	a.DB, err = gorm.Open("postgres", connectionString)
+	if err != nil {
+		fmt.Printf("Cannot connect to the database %s\n", DbName)
+		log.Fatal("Error:", err)
+	} else {
+		fmt.Printf("Connected to the database %s\n", DbName)
+	}
+
+	a.DB.Debug().AutoMigrate(&models.User{})
+
+	a.Router = mux.NewRouter().StrictSlash(true)
+
+	a.InitializeRoutes()
+}
+
+func (a *App) InitializeRoutes() {
+	a.Router.HandleFunc("/", home).Methods("GET")
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	responses.JSON(w, http.StatusOK, "Welcome to Todo App!")
+}
---
+++ api/models/user.go
@@ -0,0 +1,102 @@
+package models
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/badoux/checkmail"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	gorm.Model
+	Email string `gorm:"type:varchar(100);unique_index" json:"email"`
+	FirstName string `gorm:"size:100;not null" json:"first_name"`
+	LastName string `gorm:"size:100;not null" json:"last_name"`
+	Password string `gorm:"size:100;not null" json:"password"`
+}
+
+func HashPassword(password string) (string, error) {
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(hashPassword), err
+}
+
+func CheckPasswordHash(password, hash string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return errors.New("incorrect password")
+	}
+	return nil
+}
+
+// BeforeSave Hook is called automatically
+func (u *User) BeforeSave() error {
+	password := strings.TrimSpace(u.Password)
+	hashedPassword, err := HashPassword(password)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) PrepareUser() {
+	u.Email = strings.TrimSpace(u.Email)
+	u.FirstName = strings.TrimSpace(u.FirstName)
+	u.LastName = strings.TrimSpace(u.LastName)
+}
+
+func (u *User) ValidateUser(action string) error {
+	switch strings.ToLower(action) {
+	case "login":
+		if u.Email == "" {
+			return errors.New("email is required")
+		}
+		if u.Password == "" {
+			return errors.New("password is required")
+		}
+		return nil
+	default:
+		if u.FirstName == "" {
+			return errors.New("first_name is required")
+		}
+		if u.LastName == "" {
+			return errors.New("last_name is required")
+		}
+		if u.Email == "" {
+			return errors.New("email is required")
+		}
+		if u.Password == "" {
+			return errors.New("password is required")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("invalid email")
+		}
+		return nil
+	}
+}
+
+func (u *User) SaveUser(db *gorm.DB) (*User, error) {
+	err := db.Debug().Create(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return u, nil
+}
+
+func (u *User) GetUser(db *gorm.DB) (*User, error) {
+	user := &User{}
+	if err := db.Debug().Table("users").Where("email = ?", u.Email).Find(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (u *User) GetAllUsers(db *gorm.DB) (*[]User, error) {
+	users := []User{}
+	if err := db.Debug().Table("users").Find(&users).Error; err != nil {
+		return &[]User{}, err
+	}
+	return &users, nil
+}
